e8e1804cbbe877b4bfe5785eb896e5590973ce1a
omo2000 <64438843+omo2000@users.noreply.github.com>
2023-05-06 17:23:52 +0400

    **

---
+++ Assets/__Gameplay/Code/ResourceManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceManager : MonoBehaviour
+{
+    public Slider Oxigen;
+    public Slider Energy;
+    [SerializeField] private PlayerController PlayerController;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (PlayerController.is2003)
+        {
+            Oxigen.value++;
+            Energy.value--;
+        }
+        else
+        {
+            Oxigen.value--;
+            Energy.value++;
+        }
+                
+    }
+}
