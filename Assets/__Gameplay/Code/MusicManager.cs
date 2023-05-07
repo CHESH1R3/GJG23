f0af9720373ed44d6c1754f7b1b4d4acd429fc0d
Zurach STANiMA <zuras3374@gmail.com>
2023-05-07 09:06:18 +0400

    everything big push

---
+++ Assets/__Gameplay/Code/MusicManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public float transitionSpeed;
+    public AudioSource a2003, a2043;
+
+    public bool is2003;
+
+    private void Update()
+    {
+        Transition();
+    }
+
+    void Transition()
+    {
+        if (is2003)
+        {
+            if (a2003.volume != 1) a2003.volume = Mathf.Lerp(a2003.volume, 1, transitionSpeed * Time.deltaTime);
+            if (a2043.volume != 0) a2043.volume = Mathf.Lerp(a2043.volume, 0, transitionSpeed * Time.deltaTime);
+        }
+        else
+        {
+            if (a2003.volume != 0) a2003.volume = Mathf.Lerp(a2003.volume, 0, transitionSpeed * Time.deltaTime);
+            if (a2043.volume != 1) a2043.volume = Mathf.Lerp(a2043.volume, 1, transitionSpeed * Time.deltaTime);
+        }
+    }
+}
