b2c57d739191f913133f69f6d17b0180d9a1bf5f
omo2000 <64438843+omo2000@users.noreply.github.com>
2023-05-07 13:11:45 +0400

    ******

---
+++ Assets/__Gameplay/Code/manager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class manager : MonoBehaviour
+{
+    public GameObject GameOver;
+    public PlayerController PlayerController;
+    void Start()
+    {
+       
+    }
+
+
+    void Update()
+    {
+        
+    }
+    void Restart()
+    {
+    
+        
+    }
+}
